ca5852ec22afaf8fd97c8e086d34061fd389504d
kaeflint <esseljojo1990@gmail.com>
2023-02-14 09:45:47 +0900

    updates

--- train-bart-xsum.sh
+++ train-bart-xsum.sh
@@ -11,7 +11,8 @@ nohup python trainer.py \
 --per_device_eval_batch_size 12 \
 --save_total_limit 1 \
 --model_base  facebook/bart-base \
---run_id bart_base_model_full_e10a4 \
+--run_id bart_base_model_full_e10a4fp16 \
 --is-not-auto-encoder-data \
---gradient_accumulation_steps 4 \
---output_dir trained_models_sum/  >> training_logs_bart_base_xsum.out &
+--fp16 \
+--gradient-accumulation-steps 4 \
+--output_dir trained_models_sum/  >> training_logs_bart_base_xsum_fp16a.out &
--- train-bart.sh
+++ train-bart.sh
@@ -4,9 +4,11 @@ nohup python trainer.py \
 --eval_steps 1000 \
 --lr_scheduler_type cosine \
 --learning_rate 5e-5 \
---warmup_ratio 0.30 \
---per_device_train_batch_size 10 \
---per_device_eval_batch_size 10 \
+--warmup_ratio 0.15 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
+--fp16 \
+--gradient-accumulation-steps 4 \
 --save_total_limit 1 \
 --model_base  facebook/bart-base \
 --run_id bart_base_model_full \
